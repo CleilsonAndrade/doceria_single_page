e97ceb8b812e1427af20f5895204052bd4a017e9
CleilsonAndrade <cleilsonjose@hotmail.com>
2022-09-21 09:19:21 -0300

    :tada: first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// function scrollTo(element){
+//   document.querySelector(element).scrollIntoView({ behavior: 'smooth' })
+// }
+
+// document.getElementById('sessao-contato').addEventListener('click', function(event){
+//   event.preventDefault()
+
+//   scrollTo('#sessao-bemvindo')
+//   console.log('Teste')
+// })
+
+document.getElementById('sessao-contato').addEventListener('click', function(){
+  console.log('tezt')
+})
